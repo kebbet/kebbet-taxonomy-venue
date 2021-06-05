e2923acdd38709871e089c91c82ff95be6601ff6
kebbet <info@verkan.se>
2021-06-05 21:28:04 +0200

    Add tax to more post types.

--- kebbet-taxonomy-venue.php
+++ kebbet-taxonomy-venue.php
@@ -16,7 +16,7 @@
 namespace kebbet\taxonomy\venue;
 
 const TAXONOMY   = 'venue';
-const POST_TYPES = array( 'post' );
+const POST_TYPES = array( 'post', 'event-archive' );
 const HIDE_SLUG  = false;
 
 /**
